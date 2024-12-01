f54dcfcba0ec483261fd3ddfc0830c88ec3f4d1f
SDK <144082400+SDeeKey@users.noreply.github.com>
2024-12-01 22:17:20 +1000

    + Button

--- src/shared/ui-kit/button/button.tsx
+++ src/shared/ui-kit/button/button.tsx
@@ -1,11 +1,26 @@
+'use client'
 import React from 'react';
-import styles from './button.module.scss';
+import s from './button.module.scss';
 
-const Button = ({title}: {title: string}) => {
+
+interface ButtonProps {
+    title: string;
+    anchorId: string;
+}
+
+const Button = ({title, anchorId}: ButtonProps) => {
+
+    const handleClick = () => {
+        const anchor =  document.getElementById(anchorId);
+        if (anchor) {
+            anchor.scrollIntoView({behavior: 'smooth'});
+        }
+    }
+
     return (
         <div>
-             <button className={styles.btn}>
-                <sub className='subtitleSB'>{title}</sub>
+            <button className={`${s['btn']} subtitle-sb`} onClick={handleClick}>
+                {title}
             </button>
         </div>
     );
