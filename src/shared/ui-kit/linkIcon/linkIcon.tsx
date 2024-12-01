a219747268052d4440a2977a5c72fa13eee0c009
SDK <144082400+SDeeKey@users.noreply.github.com>
2024-12-01 22:28:52 +1000

    + linkIcon

--- src/shared/ui-kit/linkIcon/linkIcon.tsx
+++ src/shared/ui-kit/linkIcon/linkIcon.tsx
@@ -1,29 +1,20 @@
 import React from 'react';
-import styles from './linkIcon.module.scss';
+import clsx from 'clsx';
+import LinkIconSvg from './../../../img/linkIcon.svg';
+import s from './linkIcon.module.scss'
 
-// Определяем типы для пропсов
 interface LinkIconProps {
     link: string;
     size: 'md' | 'lg';
 }
 
-const LinkIcon: React.FC<LinkIconProps> = ({ link, size }) => {
-    const sizes: Record<string, React.CSSProperties> = { // Определяем тип объекта, который используем
-        md: { height: '36px', width: '36px' },
-        lg: { height: '44px', width: '44px' },
-    };
-
-    // Выбор стиля в зависимости от размера
-    const style = sizes[size] || sizes.md; // По умолчанию размер md
-
+const LinkIcon = ({link, size = 'md'}: LinkIconProps) => {
     return (
-        <div>
-            <a href={link} target='_blank' rel='noopener noreferrer'>
-                <div className={styles.linkIconContainer} style={style}>
-                    <img src="linkIcon.svg" alt="linkIcon" />
-                </div>
-            </a>
-        </div>
+        <a href={link} target='_blank' rel='noopener noreferrer' >
+            <div className={clsx(s['button'], s[`button--${size}`])}>
+                <LinkIconSvg />
+            </div>
+        </a>
     );
 };
 
