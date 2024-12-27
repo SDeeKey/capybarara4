33de0559362ae04f81aa78977618331ae67f7197
SDK <144082400+SDeeKey@users.noreply.github.com>
2024-12-27 20:51:37 +1000

    теперь точно последний

---
+++ src/shared/navigationMenu/MenuItemsArray.ts
@@ -0,0 +1,20 @@
+type MenuItemsArray = {
+    menuItem: string;
+    anchor?: string;
+}
+
+export const NavigationItems: MenuItemsArray[]= [
+    {
+        menuItem: 'Experience',
+    },
+    {
+        menuItem: 'Work',
+    },
+    {
+        menuItem: 'Testimonials',
+    },
+    {
+        menuItem: 'Contact',
+    },
+
+]
