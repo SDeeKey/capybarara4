fdacc4e55096684ad91fb50a3a77ed69f3fa7c53
SDK <144082400+SDeeKey@users.noreply.github.com>
2025-02-11 18:41:22 +1000

    change testimonials

--- src/shared/test/testimonialsList.tsx
+++ src/shared/test/testimonialsList.tsx
@@ -8,17 +8,27 @@ import clsx from "clsx";
 
 export const TestimonialsList = () => {
     const [currentIndex, setCurrentIndex] = useState(0);
+
     useEffect(() => {
+        // Интервал в 5 секунд
         const interval = setInterval(() => {
+            // Обновляем currentIndex, увеличивая его на 1
+            // Если индекс достигает длины массива feedbackDataItem,
+            // он сбрасывается на 0 благодаря операции %
             setCurrentIndex(prev => (prev + 1) % feedbackDataItem.length);
         }, 5000);
+        // Очищаем интервал при размонтировании компонента, чтобы избежать утечек памяти
         return () => clearInterval(interval);
 
     }, []);
+
+    // Переупорядочиваем массив feedbackDataItem так, чтобы текущий элемент (currentIndex)
+    // был первым в массиве, а остальные элементы следовали за ним
     const reorderedItems = [
-        ...feedbackDataItem.slice(currentIndex),
-        ...feedbackDataItem.slice(0, currentIndex)
+        ...feedbackDataItem.slice(currentIndex),  // Берем часть массива от currentIndex до конца
+        ...feedbackDataItem.slice(0, currentIndex) // Добавляем часть массива от начала до currentIndex
     ];
+
     return (
         <div className={s['testimonials-list']}>
             {reorderedItems.map((item, index) => (
