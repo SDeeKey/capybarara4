3ec27f92d82e6b373044235af25021088c89351c
SDK <144082400+SDeeKey@users.noreply.github.com>
2025-02-14 00:12:21 +1000

    change api

--- src/entities/userData/userDataApi.ts
+++ src/entities/userData/userDataApi.ts
@@ -2,11 +2,8 @@ import {UserDataDto} from "@/entities/userData/userDataDto";
 import {baseApi} from "@/entities/globalConfig";
 
 export const sendUserData = async (userData: UserDataDto) => {
-    try {
-        const response = await baseApi.post('/send-request', userData);
-        return response.data;
-    } catch (error) {
-        console.error('Ошибка при отправке данных:', error);
-        throw error;
-    }
+
+await baseApi.post('/send-request', userData)
+    .then(res=> console.log('Данные отправлены', res))
+    .catch(err => console.log('ошибка',err.message));
 };
